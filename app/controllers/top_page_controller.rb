9d8e1a80e15d61baf8ba136e664ace9f959e68e2
shiro <shiro.mns@gmail.com>
2019-07-28 16:06:33 +0900

    [追加]ソラニワ用にレイアウト調整

--- app/controllers/top_page_controller.rb
+++ app/controllers/top_page_controller.rb
@@ -1,4 +1,6 @@
 class TopPageController < ApplicationController
   def index
+    @latest_created = Status.maximum("created_at")
+    @uploaded_created = UploadedCheck.maximum("created_at")
   end
 end
