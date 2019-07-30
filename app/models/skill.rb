5e7e4166fb51c23d0233416ddf9181be35778a68
shiro <shiro.mns@gmail.com>
2019-07-30 12:00:39 +0900

    [追加]スキル検索にステータス・キャラクターのタイプでの絞り込み

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,6 @@
 class Skill < ApplicationRecord
 	belongs_to :pc_name, :foreign_key => [:e_no], :primary_key => [:e_no], :class_name => 'Name'
+	belongs_to :status, :foreign_key => [:e_no, :created_at], :primary_key => [:e_no, :created_at], :class_name => 'Status'
 	belongs_to :type,   :foreign_key => :type_id,   :primary_key => :type_id,   :class_name => 'TypeName'
 	belongs_to :nature, :foreign_key => :nature_id, :primary_key => :proper_id, :class_name => 'ProperName'
 	belongs_to :timing, :foreign_key => :timing_id, :primary_key => :proper_id, :class_name => 'ProperName'
