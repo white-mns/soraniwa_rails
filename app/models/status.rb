c374863dcf589a0805ce898d36dc8464119dc374
shiro <shiro.mns@gmail.com>
2019-09-15 13:38:50 +0900

    [追加]ステータスグラフ

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,4 +2,10 @@ class Status < ApplicationRecord
 	belongs_to :pc_name, :foreign_key => [:e_no], :primary_key => [:e_no], :class_name => 'Name'
 	belongs_to :type,   :foreign_key => :type_id,   :primary_key => :type_id, :class_name => 'TypeName'
 	belongs_to :fan_of_flower, :foreign_key => :fan_of_flower_id, :primary_key => :proper_id, :class_name => 'ProperName'
+
+    # 人数グラフの表示
+    scope :to_sum_graph_type_id, -> (column) {
+        propers_name = Hash[*TypeName.pluck(:type_id, :name).flatten]
+        pluck(column).inject(Hash.new(0)){|hash, a| hash[propers_name[a]] += 1 ; hash}.sort_by{ |k, v| v}
+    }
 end
