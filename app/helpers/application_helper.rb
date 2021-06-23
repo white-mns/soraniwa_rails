8c6b0483179e88d2234d07bc07e407bd5d93d902
shiro <shiro.mns@gmail.com>
2021-06-23 18:13:48 +0900

    [変更]Stroll Green -restroll-開始に伴い、旧データのデータ小屋として各種文言・リンク変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def page_title
-        title = "ソラニワデータ小屋"
+        title = "ソラニワGFデータ小屋"
         title = @page_title + " | " + title if @page_title
         title
     end
